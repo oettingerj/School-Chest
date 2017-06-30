e2932a0de557f6afc3b9cf800ff7f658580cdca9
oettingerj <Josh@joshs-macbook-pro-6.local>
2017-06-29 20:04:52 -0500

    Initial Commit

---
+++ School Chest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  School Chest
+//
+//  Created by Josh Oettinger on 6/29/17.
+//  Copyright © 2017 Josh Oettinger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
